2d5a95e2863e01b22960bdc0d61fd7dc4e4a93d7
Aleksandr Kuptsov <kuptsov.sasha@mail.ru>
2025-06-30 16:25:57 +0300

    add tests

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,28 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import stylistic from '@stylistic/eslint-plugin';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js, '@stylistic': stylistic  },
+    extends: ['js/recommended'],
+    rules: {
+      '@stylistic/semi': 'off',
+      '@stylistic/quotes': 'off',
+      '@stylistic/comma-dangle': 'off',
+      '@stylistic/arrow-parens': 'off',
+      '@stylistic/no-multiple-empty-lines': 'off',
+      '@stylistic/brace-style': 'off',
+      '@stylistic/object-curly-spacing': 'off',
+      '@stylistic/indent': 'off',
+      '@stylistic/no-trailing-spaces': 'off',
+      '@stylistic/padded-blocks': 'off',
+    },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
 ]);
