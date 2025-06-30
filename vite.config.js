bcf8b356ca2396701fb4492fc794f9fa0b977e44
Aleksandr Kuptsov <kuptsov.sasha@mail.ru>
2025-06-30 19:33:49 +0300

    add tests

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,20 @@
 import { defineConfig } from 'vite'
-import {path, __dirname} from 'path'
+import path from 'path'
 
 export default defineConfig({
   root: path.resolve(__dirname, 'src'),
   resolve: {
     alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap')
-    }
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    },
   },
   server: {
     port: 8080,
     hot: true,
-    open: true
+    open: true,
   },
   build: {
     outDir: path.resolve(__dirname, 'dist'),
     emptyOutDir: true,
   },
 })
-
