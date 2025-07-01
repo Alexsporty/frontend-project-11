e0dfc76ee6bf408fd6b6203d90d70d53feae82ff
Aleksandr Kuptsov <kuptsov.sasha@mail.ru>
2025-07-01 11:10:18 +0300

    add tests

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,17 @@
 import js from '@eslint/js'
-import stylistic from '@stylistic/eslint-plugin'
 import globals from 'globals'
 import { defineConfig } from 'eslint/config'
+import { includeIgnoreFile } from '@eslint/compat'
+import stylistic from '@stylistic/eslint-plugin'
+import { fileURLToPath } from 'url'
+
+const gitIgnorePath = fileURLToPath(new URL('.gitignore', import.meta.url))
+const eslintIgnorePath = fileURLToPath(new URL('.eslintignore', import.meta.url))
 
 export default defineConfig([
-  {
-    files: ['**/*.{js,mjs,cjs}'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
-    },
-    plugins: {
-      '@stylistic': stylistic,
-    },
-    rules: {
-      // базовые правила
-      ...js.configs.recommended.rules,
-
-      // стилистика (можно отключать или настраивать ниже)
-      'semi': ['error', 'never'],
-      '@stylistic/semi': ['error', 'never'],
-      '@stylistic/quotes': ['error', 'single'],
-      '@stylistic/comma-dangle': ['error', 'only-multiline'],
-      '@stylistic/arrow-parens': ['error', 'as-needed'],
-      '@stylistic/object-curly-spacing': ['error', 'always'],
-      '@stylistic/brace-style': ['error', '1tbs'],
-      '@stylistic/no-multiple-empty-lines': ['error', { max: 1 }],
-      '@stylistic/no-trailing-spaces': 'error',
-      '@stylistic/padded-blocks': ['error', 'never'],
-    },
-  },
+  includeIgnoreFile(gitIgnorePath),
+  includeIgnoreFile(eslintIgnorePath),
+  stylistic.configs.recommended,
+  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.browser } },
 ])
--- src/state.js
+++ src/state.js
@@ -6,4 +6,3 @@ export const state = {
     readPostId: new Set(),
   },
 }
-console.log(state)
